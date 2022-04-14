2998766fda98dc0391ffe4479fbb8fa2e5256ea3
Jyoti Gurung <74558610+Jyoti-Gurung@users.noreply.github.com>
2022-04-14 22:04:53 +0000

    Comments

--- main.cpp
+++ main.cpp
@@ -1,22 +1,22 @@
 /*
 Name: Jyoti Gurung
 Date: 4/12/22
-Assignment: Making an Max Heap (Pyramid with the best on the top), up to 100 #s with values up to 1000.
+Assignment: Making an Max Heap (Pyramid with the largest # on the top), up to 100 #s with values up to 1000.
 */
 
 #include <iostream>
 
 using namespace std;
 
+// Set up functions up here; fleshed out under main Driver
 void buildHeap(int * input, int n);
 void heapify(int * input, int n, int i);
 void display(int * input, int size, int depth, int control);
 
 int main(){
   
-  int storage[100];
-  
-  int count = 0;
+  int storage[100]; // 100 numbers storage
+  int count = 0; // Amount of actual numbers in storage
   char action;
 
   while (true) {
@@ -26,7 +26,7 @@ int main(){
 
     switch (action) {
 
-      //add
+      //Add
       case 'a':
 
         int input;
@@ -38,14 +38,14 @@ int main(){
         }
         if (count > 100) {
           cout << "Max 100 #s in this heap!" << endl;            break;
-          }
+        }
 
         count++;
         storage[count-1] = input;
           
         break;
 
-      //print
+      //Print
       case 'b':
         buildHeap(storage, count);
         display(storage, count, 1, 1);
@@ -61,12 +61,10 @@ int main(){
 //Skeleton for Max Heap
 void buildHeap(int * input, int size)
 {
-    // Index of last non-leaf node
+    // Index of LAST parent branch, like the one that has baby branches
     int starter = (size / 2) - 1;
  
-    // Perform reverse level order traversal
-    // from last non-leaf node and heapify
-    // each node
+    // Heapify all the PARENT branches, in reverse order; aka from bottom and in right to left motion
     for (int i = starter; i >= 0; i--) {
         heapify(input, size, i);
     }
@@ -79,27 +77,33 @@ void heapify(int * input, int size, int i)
     int left = 2 * i + 1; 
     int right = 2 * i + 2;
  
-    // If left child is larger than root
+    // If left child is larger than root, root is changed to value of left
     if (left < size && input[left] > input[root])
         root = left;
  
-    // If right child is larger than root
+    // If right child is larger than root, root is changed to value of right
     if (right < size && input[right] > input[root])
         root = right;
  
-    // If largest is not root swap em, and call this function again to heapify branches
+    // If largest is not root swap them, and call this function again to heapify branches
     if (root != i) {
         swap(input[i], input[root]);
         heapify(input, size, root);
     }
 }
 
+// Print out the arrays that is already sorted
 void display(int * input, int size, int depth, int control) {
   
   for (int i = 0; i < size; i++) {
+    
     cout << input[i] << " ";
+    
+    // If the current slot is equal to depth we bring       it down a line
     if (i+1 == depth) {
+      // First, Double Control aka each branch has 2          spots for babies
       control = control * 2;
+      // Second, add it to depth, which is collecting         the # of slots from previous depths
       depth = depth+control;
       cout << endl;
     }
